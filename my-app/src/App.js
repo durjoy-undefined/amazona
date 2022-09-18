7f91d7917da178be17a2d0a214d2276f48c61009
durjoy-undefined <durjoyundefined@gmail.com>
2022-09-18 15:24:56 +0600

    amazona web app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+
+      <main>list items</main>
+    </div>
+  );
+}
+
+export default App;
